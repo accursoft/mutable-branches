49170184d34654dd003c33672acfea88b907e796
Gideon Sireling <code@accursoft.com>
2012-11-19 16:16:03 +0200

    suppress "branches are permanent and global" warning

--- mutable-branches.py
+++ mutable-branches.py
@@ -6,5 +6,19 @@ If the branch name contains spaces, it should be quoted.
 Only the most recently checked in version of .hgbranches is used.
 """
 
+from mercurial import dispatch, extensions, commands
+
+def uisetup(ui):
+    extensions.wrapcommand(commands.table, 'branch', branch_wrapper)
+
+def branch_wrapper(orig, ui, *args, **kwargs):
+    ui.pushbuffer()
+    ret = orig(ui, *args, **kwargs)
+    out = ui.popbuffer()
+    lines = out.splitlines(True)
+    if lines[-1].startswith("(branches are permanent and global"): lines.pop()
+    ui.write(''.join(lines))
+    return ret
+
 testedwith = '2.3 2.4'
 buglink = 'http://code.accursoft.com/mutable-branches/issues'
